8d42d42e9ad2cd98f0c6304026986b148b9e4482
Romera Matias <romeramatias0@gmail.com>
2020-10-20 21:22:55 -0300

    Edit Desafio 2

--- proyecto-express/models/User.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require("mongodb");
-
-const userSchema = new Schema ({
-    username: String,
-    email: String,
-    password: String
-})
-
-module.exports = model('User', userSchema)
